d71030201645ca1bb3fb2279e5d59d249be46d91
ptasha25 <112662625+ptasha25@users.noreply.github.com>
2023-05-12 20:44:04 +0300

    Update Game_Snake.py

--- Game_Snake.py
+++ Game_Snake.py
@@ -9,6 +9,8 @@ class Game:
     pygame.init()
     __size_of_field = 1000
     __size_of_object = 50
+    __rad_of_eyes = 5
+    __circul_of_eyes = 2
     _surface = pygame.display.set_mode((__size_of_field, __size_of_field))
     _menu = pygame_menu.Menu('SNAKE', __size_of_field, __size_of_field,
                                   theme=pygame_menu.themes.THEME_BLUE)
@@ -43,20 +45,14 @@ class Game:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
                         its_writen = True
-                        #return user_text
                         break
 
-                # when mouse collides with the rectangle
-                # make active as true
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if input_rect.collidepoint(event.pos):
                         active = True
 
-                # if the key is physically pressed down
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_BACKSPACE:
-
-                        # stores text except last letter
                         user_text = user_text[0:-1]
                     else:
                         user_text += event.unicode
@@ -70,7 +66,6 @@ class Game:
 
             pygame.draw.rect(display_screen, color, input_rect)
 
-            # render the text
             text_surface = my_font.render('Your name:', 1, ("lightskyblue"))
             display_screen.blit(text_surface, (0, 0))
             pygame.display.flip()
@@ -97,54 +92,62 @@ class Game:
             result = cur.fetchall()
             bd.commit()
         cur.close()
-        #bd.close()
     @staticmethod
     def pressing_the_button(dx, dy):
-        dris = {'W': True, 'S': True, 'A': True, 'D': True}
+        dir = {'W': True, 'S': True, 'A': True, 'D': True}
         key = pygame.key.get_pressed()
-        if key[pygame.K_w] and dris['W']:
-            dris = {'W': True, 'S': False, 'A': True, 'D': True}
+        if key[pygame.K_w] and dir['W']:
+            dir = {'W': True, 'S': False, 'A': True, 'D': True}
             dx, dy = 0, -1
-        if key[pygame.K_s] and dris['S']:
+        if key[pygame.K_s] and dir['S']:
             dx, dy = 0, 1
-            dris = {'W': False, 'S': True, 'A': True, 'D': True}
-        if key[pygame.K_a] and dris['A']:
+            dir = {'W': False, 'S': True, 'A': True, 'D': True}
+        if key[pygame.K_a] and dir['A']:
             dx, dy = -1, 0
-            dris = {'W': True, 'S': True, 'A': True, 'D': False}
-        if key[pygame.K_d] and dris['D']:
+            dir = {'W': True, 'S': True, 'A': True, 'D': False}
+        if key[pygame.K_d] and dir['D']:
             dx, dy = 1, 0
-            dris = {'W': True, 'S': True, 'A': False, 'D': True}
+            dir = {'W': True, 'S': True, 'A': False, 'D': True}
         return dx, dy
 
     @staticmethod
-    def set_colors(sc, SIZE, snake, apple, score, small_cloud_1, small_cloud_2, small_cloud_3, small_cloud_4):
+    def set_colors(sc, SIZE, snake, apple, score, cloud):
         font_score = pygame.font.SysFont('Arial', 26, bold=True)
         [(pygame.draw.rect(sc, pygame.Color('green'), (i, j, SIZE - 2, SIZE - 2))) for i, j in snake]
+        pygame.draw.circle(sc, pygame.Color("black"), (snake[-1][0] + SIZE // 2 - 10, snake[-1][1] + SIZE // 2), Game.__rad_of_eyes, Game.__circul_of_eyes)
+        pygame.draw.circle(sc, pygame.Color("black"), (snake[-1][0] + SIZE // 2 + 10, snake[-1][1] + SIZE // 2), Game.__rad_of_eyes, Game.__circul_of_eyes)
         pygame.draw.rect(sc, pygame.Color('red'), (*apple, SIZE, SIZE))
-        pygame.draw.rect(sc, pygame.Color('white'), (*small_cloud_1, SIZE, SIZE))
-        pygame.draw.rect(sc, pygame.Color('white'), (*small_cloud_2, SIZE, SIZE))
-        pygame.draw.rect(sc, pygame.Color('white'), (*small_cloud_3, SIZE, SIZE))
-        pygame.draw.rect(sc, pygame.Color('white'), (*small_cloud_4, SIZE, SIZE))
+        for i in range (len(cloud)):
+            pygame.draw.rect(sc, pygame.Color('white'), (*cloud[i][0], SIZE, SIZE))
+            pygame.draw.rect(sc, pygame.Color('white'), (*cloud[i][1], SIZE, SIZE))
         render_score = font_score.render(f'SCORE: {score}', 1, pygame.Color('orange'))
         sc.blit(render_score, (5, 5))
+
+    def red_eyes(indexes, size, snake, sc, SIZE):
+        if (size > 1):
+            pygame.draw.circle(sc, pygame.Color("red"),
+                               (snake[-2][0] + SIZE // 2 - 10, snake[-2][1] + SIZE // 2), Game.__rad_of_eyes)
+            pygame.draw.circle(sc, pygame.Color("red"),
+                               (snake[-2][0] + SIZE // 2 + 10, snake[-2][1] + SIZE // 2), Game.__rad_of_eyes)
+        else:
+            pygame.draw.circle(sc, pygame.Color("red"),
+                               (indexes[0] + SIZE // 2 - 10, indexes[1] + SIZE // 2), Game.__rad_of_eyes)
+            pygame.draw.circle(sc, pygame.Color("red"),
+                               (indexes[0] + SIZE // 2 + 10, indexes[1] + SIZE // 2), Game.__rad_of_eyes)
 
     def start_the_game():
         RES = Game.__size_of_field
         SIZE = Game.__size_of_object
         END = 200
+        number_of_clouds = 4
         x, y = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
         apple = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
-        small_cloud_1 = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
-        if small_cloud_1 == apple:
-            small_cloud_1 = (small_cloud_1[0] + SIZE, small_cloud_1[1])
-        small_cloud_2 = (small_cloud_1[0] + SIZE, small_cloud_1[1])
-        cloud = []
-        cloud.append([small_cloud_1, small_cloud_2])
-        small_cloud_3 = randrange(0, RES, SIZE), randrange(0, RES, SIZE)
-        if small_cloud_3 == apple:
-            small_cloud_3 = (small_cloud_3[0] + SIZE, small_cloud_3[1])
-        small_cloud_4 = (small_cloud_3[0] + SIZE, small_cloud_3[1])
-        cloud.append([small_cloud_3, small_cloud_4])
+        cloud = [[], [], [], []]
+        for i in range(number_of_clouds):
+            cloud[i].append((randrange(0, RES, SIZE), randrange(0, RES, SIZE)))
+            if cloud[i][0] == apple:
+                cloud[i][0] = (cloud[i][0][0] + SIZE, cloud[i][0][1])
+            cloud[i].append((cloud[i][0][0] + SIZE, cloud[i][0][1]))
         length = 1
         snake = [(x, y)]
         score = 0
@@ -158,8 +161,9 @@ class Game:
         while True:
             sc.fill(pygame.Color('blue'))
 
-            Game.set_colors(sc, SIZE, snake, apple, score, small_cloud_1, small_cloud_2, small_cloud_3, small_cloud_4)
+            Game.set_colors(sc, SIZE, snake, apple, score, cloud)
 
+            pre_for_one = (x, y)
             x += dx * SIZE
             y += dy * SIZE
             snake.append((x, y))
@@ -174,6 +178,7 @@ class Game:
             in_cloud = False
             for clouds in cloud:
                 if clouds[0] == snake[-1] or clouds[1] == snake[-1]:
+                    Game.red_eyes(pre_for_one, len(snake), snake, sc, SIZE)
                     render_end = font_end.render('GAME OVER', 1, pygame.Color('orange'))
                     sc.blit(render_end, (RES // 2 - END, RES // 3))
                     pygame.display.flip()
@@ -185,6 +190,7 @@ class Game:
 
 
             if x < 0 or x > RES - SIZE or y < 0 or y > RES - SIZE or len(snake) != len(set(snake)):
+                Game.red_eyes(pre_for_one, len(snake), snake, sc, SIZE)
                 render_end = font_end.render('GAME OVER', 1, pygame.Color('orange'))
                 sc.blit(render_end, (RES // 2 - END, RES // 3))
                 pygame.display.flip()
@@ -199,7 +205,6 @@ class Game:
                     return
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q:
-                        #pygame.quit()
                         return
 
             dx, dy = Game.pressing_the_button(dx, dy)
